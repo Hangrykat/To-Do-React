380488319a01109b7a8fdb9df21391e21bddfdfa
Hsueh Yen Neo <neo.hsuehyen@gmail.com>
2020-07-22 15:47:18 +0200

    skeleton with main folders of  components

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import './App.css';
 
-import Header from '../components/header/Header'
-import MainThing from '../components/mainThing/MainThing';
+import Header from '../components/header/Header';
+import Main from '../components/main/Main';
 import Footer from '../components/footer/Footer';
 
 
 function App() {
-  return (
-    <div>
-      <h1>Hi</h1>
-      <Header />
-      <Body />
-      <Footer />
-    </div>
-    /*
+  return (   
     <div className="App">
       <Header />
-      <MainThing />
+      <Main />
       <Footer />
-    </div>*/
+    </div>
   );
 }
 
