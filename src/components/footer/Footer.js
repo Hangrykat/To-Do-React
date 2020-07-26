828e2ff88a932a375859561120b88e0799bcc4a1
Bruno DeLucca <bruno@delucca.cl>
2020-07-26 19:41:37 +0200

    All part of the program that were in javaScript are working in React. Still the focus on the <p> tag is missing, but all the rest seems to work just fine

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,31 +1,57 @@
-import React from 'react'; 
+import React from "react";
 //import styles from './Footer.module.css';
-import './Footer.css'; 
-import plusLogo from './plus.svg'
+import "./Footer.css";
+import plusLogo from "./plus.svg";
+import shortid from "shortid";
 
+class Footer extends React.Component {
+  state = {
+    text: "",
+  };
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
 
-function Footer() {
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit({
+      id: shortid.generate(),
+      text: this.state.text,
+      complete: false,
+      contentEditable: false,
+    });
+    this.setState({
+      text: "",
+    });
+  };
+
+  render() {
     return (
-        <form id="inputBox">
-            <input
-            type="text"
-            placeholder="Add a task...."
-            id="inputField"
-            autocomplete="off"
-            />
-            <button
-            onclick="newTaskList.addTask()"
-            type="submit"
-            value="Add task"
-            id="addButton"
-            >
-            <div class="addIcon"><img src={plusLogo} /></div>
-            </button>
-        </form>
-    )
+      <form id="inputBox" onSubmit={this.handleSubmit}>
+        <input
+          name="text"
+          placeholder="Add a task...."
+          id="inputField"
+          autoComplete="off"
+          value={this.state.text}
+          onChange={this.handleChange}
+        />
+        <button
+          onClick={this.handleSubmit}
+          type="submit"
+          value="Add task"
+          id="addButton"
+        >
+          <div className="addIcon">
+            <img src={plusLogo} alt="button" />
+          </div>
+        </button>
+      </form>
+    );
+  }
 }
 
 export default Footer;
-
-
