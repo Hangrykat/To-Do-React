0a32a9b9f4460b53734aa06d7648f842b1fc8c19
Bruno DeLucca <bruno@delucca.cl>
2020-07-27 16:55:22 +0200

    Change the program to a function base

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,75 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Main.css";
 import Task from "../Task/Task";
 import Footer from "../footer/Footer";
 
-class Main extends React.Component {
-  state = {
-    todos: [],
-  };
+const Main = () => {
+  const [taskContainer, setNewTask] = useState({
+    taskList: [],
+  });
+  // class Main extends React.Component {
+  //   state = {
+  //     todos: [],
+  //   };
 
-  addTodo = (todo) => {
-    this.setState({
-      todos: [...this.state.todos, todo],
+  const addTask = (task) => {
+    setNewTask({
+      taskList: [...taskContainer.taskList, task],
     });
   };
+  //   addTodo = (todo) => {
+  //     this.setState({
+  //       todos: [...this.state.todos, todo],
+  //     });
+  //   };
 
-  toggleComplete = (id) => {
-    this.setState({
-      todos: this.state.todos.map((todo) => {
-        if (todo.id === id) {
+  const toggleComplete = (id) => {
+    setNewTask({
+      taskList: taskContainer.taskList.map((task) => {
+        if (task.id === id) {
           return {
-            ...todo,
-            complete: !todo.complete,
+            ...task,
+            complete: !task.complete,
           };
         } else {
-          return todo;
+          return task;
+        }
+      }),
+    });
+  };
+  //   toggleComplete = (id) => {
+  //     this.setState({
+  //       todos: this.state.todos.map((todo) => {
+  //         if (todo.id === id) {
+  //           return {
+  //             ...todo,
+  //             complete: !todo.complete,
+  //           };
+  //         } else {
+  //           return todo;
+  //         }
+  //       }),
+  //     });
+  //   };
+
+  const handleDeleteTodo = (id) => {
+    setNewTask({
+      taskList: taskContainer.taskList.filter((task) => task.id !== id),
+    });
+  };
+
+  //   handleDeleteTodo = (id) => {
+  //     this.setState({
+  //       todos: this.state.todos.filter((todo) => todo.id !== id),
+  //     });
+  //   };
+
+  const handleEditTodo = (id) => {
+    setNewTask({
+      taskList: taskContainer.taskList.map((task) => {
+        if (task.id === id) {
+          const taskEditable = {
+            ...task,
+            contentEditable: !task.contentEditable,
+          };
+          return taskEditable;
+        } else {
+          return task;
         }
       }),
     });
   };
 
-  handleDeleteTodo = (id) => {
-    this.setState({
-      todos: this.state.todos.filter((todo) => todo.id !== id),
-    });
-  };
+  //   handleEditTodo = (id) => {
+  //     this.setState({
+  //       todos: this.state.todos.map((todo) => {
+  //         if (todo.id === id) {
+  //           //Suppose to update
+  //           const todoss = {
+  //             ...todo,
+  //             contentEditable: !todo.contentEditable,
+  //           };
+  //           return todoss;
+  //         } else {
+  //           return todo;
+  //         }
+  //       }),
+  //     });
+  //   };
 
-  handleEditTodo = (id) => {
-    this.setState({
-      todos: this.state.todos.map((todo) => {
-        if (todo.id === id) {
-          //Suppose to update
-          const todoss = {
-            ...todo,
-            contentEditable: !todo.contentEditable,
-          };
-          return todoss;
-        } else {
-          return todo;
-        }
-      }),
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <div id="taskLists">
-          {this.state.todos.map((todo) => (
-            <Task
-              toggleComplete={() => this.toggleComplete(todo.id)}
-              onDelete={() => this.handleDeleteTodo(todo.id)}
-              onEdit={() => this.handleEditTodo(todo.id)}
-              key={todo.id}
-              todo={todo}
-            />
-          ))}
-        </div>
-        <Footer onSubmit={this.addTodo} />
+  return (
+    <div>
+      <div id="taskLists">
+        {taskContainer.taskList.map((task) => (
+          <Task
+            toggleComplete={() => toggleComplete(task.id)}
+            onDelete={() => handleDeleteTodo(task.id)}
+            onEdit={() => handleEditTodo(task.id)}
+            key={task.id}
+            todo={task}
+          />
+        ))}
       </div>
-    );
-  }
-}
+      <Footer onSubmit={addTask} />
+    </div>
+  );
+};
 
 export default Main;
